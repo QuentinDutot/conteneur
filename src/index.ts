74a2302b34fb885220bc2b27b4c9e5ba01063bbf
QuentinDutot <dutot.quentin@gmail.com>
2024-09-27 16:54:38 +0200

    add biome ignore explanation on types exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // biome-ignore lint/performance/noBarrelFile: expose public apis
 export { createContainer } from './container/container'
 
-// biome-ignore lint/performance/noReExportAll: <explanation>
+// biome-ignore lint/performance/noReExportAll: expose public types
 export * from './types'
